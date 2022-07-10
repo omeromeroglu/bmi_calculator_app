047c92bf3f82319f95244c13a8cec5a0e352d97f
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-07-11 00:45:52 +0200

    added widgets and main dart

---
+++ lib/screens/home.dart
@@ -0,0 +1,36 @@
+import 'package:bmi_calculator_app/constants/app_constants.dart';
+import 'package:bmi_calculator_app/widgets/right_bar.dart';
+import 'package:bmi_calculator_app/widgets/left_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState()=> _HomeScreenState();
+};
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BMI Calculator",
+          style: TextStyle(color: accentHexColor,fontWeight: FontWeight.w300),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height:20,)
+          ]
+        )
+      )
+    );
+  }
+}
+
+
