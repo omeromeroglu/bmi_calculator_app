0565cab3d6cf0796f57ebf441fe90ad520121c18
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-08-13 23:58:33 +0200

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch : Colors.yellow,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       )
-      home: HomeScreen()
-    )
+      home: HomeScreen(),
+    );
   }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,36 +1,116 @@
 import 'package:bmi_calculator_app/constants/app_constants.dart';
 import 'package:bmi_calculator_app/widgets/right_bar.dart';
 import 'package:bmi_calculator_app/widgets/left_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
-  _HomeScreenState createState()=> _HomeScreenState();
-};
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "BMI Calculator",
-          style: TextStyle(color: accentHexColor,fontWeight: FontWeight.w300),
+        appBar: AppBar(
+          title: Text(
+            "BMI Calculator",
+            style:
+                TextStyle(color: accentHexColor, fontWeight: FontWeight.w300),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
         ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-      ),
-      backgroundColor: mainHexColor,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height:20,)
-          ]
-        )
-      )
-    );
+        backgroundColor: mainHexColor,
+        body: SingleChildScrollView(
+            child: Column(children: [
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 130,
+                child: TextField(
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.w300,
+                    color: accentHexColor,
+                  ),
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Height",
+                      hintStyle: TextStyle(
+                          fontSize: 42,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white.withOpacity(.8))),
+                ),
+              ),
+              Container(
+                width: 130,
+                child: TextField(
+                  style: TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.w300,
+                    color: accentHexColor,
+                  ),
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Weight",
+                      hintStyle: TextStyle(
+                          fontSize: 42,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white.withOpacity(.8))),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            child: Text(
+              "Calculate",
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: accentHexColor),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            child: Text(
+              "10",
+              style: TextStyle(fontSize: 90, color: accentHexColor),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+           Container(
+            child: Text(
+              "Normal weight",
+              style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                  color: accentHexColor),
+            ),
+          ),
+          SizedBox(height: 10,),
+          LeftBar(barWidth: 40),
+          SizedBox(height: 20,),
+          LeftBar(barWidth: 70),
+          SizedBox(height: 20,),
+          LeftBar(barWidth: 40),
+
+
+        ])));
   }
 }
-
-
